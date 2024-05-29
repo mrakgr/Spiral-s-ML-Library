9b9b6ee37324c2cabf370d19e56869985c539ac7
mrakgr <mrakgr@gmail.com>
2024-05-29 11:56:04 +0200

    Added an explanation of shoelace into the .ps1 file.

--- copy_static_assets.ps1
+++ copy_static_assets.ps1
@@ -1,3 +1,7 @@
+# Copies the shoelace assets into the static/bundles folder.
+# Shoelace is the web UI library used in some games.
+# https://shoelace.style/
+
 $sourcePath = "node_modules/@shoelace-style/shoelace/dist/assets/"
 $destinationPath = "ui/backend/static/bundles/shoelace/assets/"
 
@@ -5,7 +9,7 @@ if (-not (Test-Path $destinationPath)) {
     Copy-Item -Path $sourcePath -Destination $destinationPath -Force -Recurse
 }
 
-# Also works and would be more roboust, but its Windows only.
+# The following line, also works and would be more roboust, but its Windows only.
 # Robocopy $sourcePath $destinationPath /mir
 
 # symlinks didn't work for me.
