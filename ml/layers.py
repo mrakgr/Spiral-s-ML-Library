dc07262756f92ac5cdd42fe49ad1ba005942851c
mrakgr <mrakgr@gmail.com>
2024-06-03 16:51:13 +0200

    Extracting the input tensor works. We'll be able to use this to pass the inputs to the NN model represented by the graph.

--- ml/layers.py
+++ ml/layers.py
@@ -107,81 +107,96 @@ def method1(v0 : cp.ndarray, v1 : i32, v2 : i32, v3 : i32, v4 : i32, v5 : i32) -
     v32 = ']'
     return method2(v32)
 def main():
-    v0 = cp.empty(112,dtype=cp.uint8)
-    v1 = "---"
-    method0(v1)
-    del v1
+    v0 = cp.empty(128,dtype=cp.uint8)
+    v1 = cp.empty(104,dtype=cp.uint8)
+    v2 = "---"
+    method0(v2)
+    del v2
     print()
-    v2 = v0[0:0+4*4].view(cp.float32)
-    v3 = 0
-    v4 = 4
-    v5 = 1
+    v3 = v0[0:0+4*8].view(cp.float32)
+    v4 = 0
+    v5 = 4
     v6 = 1
-    v7 = 4
-    method1(v2, v3, v4, v5, v6, v7)
-    del v2, v3, v4, v5, v6, v7
+    v7 = 2
+    v8 = 4
+    method1(v3, v4, v5, v6, v7, v8)
+    del v3, v4, v5, v6, v7, v8
     print()
-    v8 = v0[16:16+4*16].view(cp.float32)
-    v9 = 0
-    v10 = 4
-    v11 = 1
-    v12 = 4
+    v9 = v0[32:32+4*16].view(cp.float32)
+    v10 = 0
+    v11 = 4
+    v12 = 1
     v13 = 4
-    method1(v8, v9, v10, v11, v12, v13)
-    del v8, v9, v10, v11, v12, v13
+    v14 = 4
+    method1(v9, v10, v11, v12, v13, v14)
+    del v9, v10, v11, v12, v13, v14
     print()
-    v14 = v0[80:80+4*8].view(cp.float32)
-    v15 = 0
-    v16 = 2
-    v17 = 1
-    v18 = 4
-    v19 = 2
-    method1(v14, v15, v16, v17, v18, v19)
-    del v14, v15, v16, v17, v18, v19
+    v15 = v0[96:96+4*8].view(cp.float32)
+    v16 = 0
+    v17 = 2
+    v18 = 1
+    v19 = 4
+    v20 = 2
+    method1(v15, v16, v17, v18, v19, v20)
+    del v15, v16, v17, v18, v19, v20
     print()
-    v20 = v0[0:0+4*4].view(cp.float32)
-    v21 = cp.random.normal(0.0,1.0,4,dtype=cp.float32) # type: ignore
-    cp.copyto(v20[0:0+4],v21[0:0+4])
-    del v20, v21
-    v22 = v0[16:16+4*16].view(cp.float32)
-    v23 = cp.random.normal(0.0,1.0,16,dtype=cp.float32) # type: ignore
-    cp.copyto(v22[0:0+16],v23[0:0+16])
-    del v22, v23
-    v24 = v0[80:80+4*8].view(cp.float32)
-    v25 = cp.random.normal(0.0,1.0,8,dtype=cp.float32) # type: ignore
-    cp.copyto(v24[0:0+8],v25[0:0+8])
-    del v24, v25
-    v26 = "Done initing."
-    method0(v26)
-    del v26
+    v21 = v0[0:0+4*8].view(cp.float32)
+    v22 = cp.random.normal(0.0,1.0,8,dtype=cp.float32) # type: ignore
+    cp.copyto(v21[0:0+8],v22[0:0+8])
+    del v21, v22
+    v23 = v0[32:32+4*16].view(cp.float32)
+    v24 = cp.random.normal(0.0,1.0,16,dtype=cp.float32) # type: ignore
+    cp.copyto(v23[0:0+16],v24[0:0+16])
+    del v23, v24
+    v25 = v0[96:96+4*8].view(cp.float32)
+    v26 = cp.random.normal(0.0,1.0,8,dtype=cp.float32) # type: ignore
+    cp.copyto(v25[0:0+8],v26[0:0+8])
+    del v25, v26
+    v27 = "Done initing."
+    method0(v27)
+    del v27
     print()
-    v27 = v0[0:0+4*4].view(cp.float32)
-    v28 = 0
-    v29 = 4
-    v30 = 1
+    v28 = v0[0:0+4*8].view(cp.float32)
+    v29 = 0
+    v30 = 4
     v31 = 1
-    v32 = 4
-    method1(v27, v28, v29, v30, v31, v32)
-    del v27, v28, v29, v30, v31, v32
+    v32 = 2
+    v33 = 4
+    method1(v28, v29, v30, v31, v32, v33)
+    del v28, v29, v30, v31, v32, v33
     print()
-    v33 = v0[16:16+4*16].view(cp.float32)
-    v34 = 0
-    v35 = 4
-    v36 = 1
-    v37 = 4
+    v34 = v0[32:32+4*16].view(cp.float32)
+    v35 = 0
+    v36 = 4
+    v37 = 1
     v38 = 4
-    method1(v33, v34, v35, v36, v37, v38)
-    del v33, v34, v35, v36, v37, v38
+    v39 = 4
+    method1(v34, v35, v36, v37, v38, v39)
+    del v34, v35, v36, v37, v38, v39
     print()
-    v39 = v0[80:80+4*8].view(cp.float32)
+    v40 = v0[96:96+4*8].view(cp.float32)
     del v0
-    v40 = 0
-    v41 = 2
-    v42 = 1
-    v43 = 4
-    v44 = 2
-    method1(v39, v40, v41, v42, v43, v44)
-    del v39, v40, v41, v42, v43, v44
+    v41 = 0
+    v42 = 2
+    v43 = 1
+    v44 = 4
+    v45 = 2
+    method1(v40, v41, v42, v43, v44, v45)
+    del v40, v41, v42, v43, v44, v45
+    print()
+    v46 = "Here is the input tensor."
+    method0(v46)
+    del v46
+    print()
+    v47 = v1[0:0+4*2].view(cp.float32)
+    del v1
+    v48 = 0
+    v49 = 2
+    v50 = 1
+    v51 = 1
+    v52 = 2
+    method1(v47, v48, v49, v50, v51, v52)
+    del v47, v48, v49, v50, v51, v52
     print()
     return 
 
