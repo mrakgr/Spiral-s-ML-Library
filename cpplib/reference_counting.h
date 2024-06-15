bf473fe41c120276d95e98992a0e5f0ded84c427
mrakgr <mrakgr@gmail.com>
2024-06-15 08:46:44 +0200

    Redesigned the shared pointer and the heap array.

--- cpplib/reference_counting.h
+++ cpplib/reference_counting.h
@@ -1,58 +1,61 @@
-template <typename T> void call_destructor(T & x) { x.~T(); }
-template <typename el, int dim> struct static_array { el v[dim]; };
-template <typename el, int dim, typename default_int> struct static_array_list { default_int length; el v[dim]; };
+template <typename T>
+void call_destructor(T &x) { x.~T(); }
+template <typename el, int dim>
+struct static_array
+{
+    el v[dim];
+};
+template <typename el, int dim, typename default_int>
+struct static_array_list
+{
+    default_int length;
+    el v[dim];
+};
 
 template <typename T>
 struct sptr
 {
-    struct sptr_base {
-        int refc;
-        T v;
-
-        sptr_base() = delete;
-        sptr_base(T & v_) : refc(1), v(v_) {};
-        sptr_base(T && v_) : refc(1), v(std::move(v_)) {};
-
-        ~sptr_base(){ this->v.dispose(); }
-    } * base;
+    T *base;
 
     sptr() : base(nullptr) {}
-    sptr(T & v) : base(new sptr_base(v)) {}
-    sptr(T && v) : base(new sptr_base(std::move(v))) {}
+    sptr(T *v) : base(v) { this->base->refc++; }
 
-    void dispose(){
+    ~sptr()
+    {
         if (this->base != nullptr && --this->base->refc == 0)
         {
             delete this->base;
         }
     }
 
-    ~sptr() { this->dispose(); }
-
-    sptr(sptr & x) {
+    sptr(sptr &x)
+    {
         this->base = x.base;
         this->base->refc++;
     }
 
-    sptr(sptr && x) {
+    sptr(sptr &&x)
+    {
         this->base = x.base;
         x.base = nullptr;
     }
 
-    sptr & operator=(sptr &x)
+    sptr &operator=(sptr &x)
     {
-        if (this->base != x.base){
-            this->dispose();
+        if (this->base != x.base)
+        {
+            delete this->base;
             this->base = x.base;
             this->base->refc++;
         }
         return *this;
     }
-    
-    sptr & operator=(sptr &&x)
+
+    sptr &operator=(sptr &&x)
     {
-        if (this->base != x.base){
-            this->dispose();
+        if (this->base != x.base)
+        {
+            delete this->base;
             this->base = x.base;
             x.base = nullptr;
         }
@@ -61,11 +64,13 @@ struct sptr
 };
 
 template <typename T, typename default_int>
-struct array {
+struct array
+{
+    int refc = 0;
     default_int length;
-    T * ptr;
+    T *ptr;
 
     array() = delete;
-    array(int l) : length(l), ptr(new T[l]) { }
-    void dispose(){ delete[] this->ptr; }
+    array(int l) : length(l), ptr(new T[l]) {}
+    ~array() { delete[] this->ptr; }
 };
