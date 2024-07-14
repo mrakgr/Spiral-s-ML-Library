81f986a3242b32df2414e8df9c4889b89a5c7257
mrakgr <mrakgr@gmail.com>
2024-07-14 13:46:10 +0200

    We'll do a Lit example in JSFiddle.

--- ui/frontend/leduc_train.ts
+++ ui/frontend/leduc_train.ts
@@ -1,6 +1,7 @@
 import { LitElement, PropertyValueMap, PropertyValues, css, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { map } from 'lit/directives/map.js';
+import {createRef, Ref, ref} from 'lit/directives/ref.js';
 import { io } from 'socket.io-client'
 import * as echarts from 'echarts';
 
@@ -32,38 +33,57 @@ class Leduc_Train_UI extends LitElement {
 
     static styles = css`
         :host {
+            padding: 50px;
             display: flex;
             flex-direction: row;
             box-sizing: border-box;
             height: 100%;
+            width: 100%;
+            /* background-color: red; */
+        }
+
+        div {
+            height: 60%;
+            width: 60%;
+            /* background-color: green; */
         }
     `
 
+    inputRef: Ref<HTMLElement> = createRef();
+    chart? : echarts.ECharts;
+
     render(){
-        return html`<canvas id="chart"></canvas>`
+        return html`<div ${ref(this.inputRef)}></div>`
     }
 
     protected firstUpdated(_changedProperties: PropertyValues): void {
-        // Create the echarts instance
-        const myChart = echarts.init(document.getElementById('main'));
+        const chart_container = this.inputRef.value;
+        if (chart_container) {
+            // Create the echarts instance
+            this.chart = echarts.init(chart_container);
+    
+            // Draw the chart
+            this.chart.setOption({
+                title: {
+                    text: 'ECharts Getting Started Example'
+                },
+                tooltip: { },
+                xAxis: {
+                    data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+                },
+                yAxis: {},
+                series: [
+                    {
+                    name: 'sales',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                    }
+                ]
+            });
 
-        // Draw the chart
-        myChart.setOption({
-            title: {
-                text: 'ECharts Getting Started Example'
-            },
-            tooltip: {},
-            xAxis: {
-                data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
-            },
-            yAxis: {},
-            series: [
-                {
-                name: 'sales',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-                }
-            ]
-        });
+            // Creates the resize observer for the div container. Without this the chart sizing wouldn't work properly.
+            const chart_container_resize_observer = new ResizeObserver(() => this.chart?.resize());
+            chart_container_resize_observer.observe(chart_container)
+        }
     }
 }
