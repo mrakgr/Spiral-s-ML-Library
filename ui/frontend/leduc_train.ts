867c5d154c35094c97598dc80aed56d97891da4f
mrakgr <mrakgr@gmail.com>
2024-07-15 14:42:46 +0200

    Finally we have the basic grasp of dynamic updates with ECharts.

--- ui/frontend/leduc_train.ts
+++ ui/frontend/leduc_train.ts
@@ -53,6 +53,21 @@ class Leduc_Train_UI extends LitElement {
     }
 }
 
+type Leduc_Train_Label = string
+
+type Leduc_Train_Serie = {
+    name: string
+    data: number[]
+}
+
+const update_chart = (chart: echarts.ECharts, labels: Leduc_Train_Label[], series: Leduc_Train_Serie[]) => {
+    if (series.every(x => labels.length === x.data.length) === false) { throw Error("The data sizes are not valid.") }
+    chart.setOption({
+        xAxis: { data: labels },
+        series: series.map(x => ({ ...x, type: "line" }))
+    })
+}
+
 @customElement('training-chart')
 class Training_Chart extends LitElement {
     static styles = css`
@@ -62,34 +77,55 @@ class Training_Chart extends LitElement {
             width: 100%;
         }
     `
-    
+
+    labels: Leduc_Train_Label[] = []
+    series: Leduc_Train_Serie[] = [
+        { name: "Agent 1", data: [] },
+        { name: "Agent 2", data: [] },
+        { name: "Agent 3", data: [] },
+    ]
+
     chart?: echarts.ECharts;
 
     render() {
-        return html`<slot></slot>`; // Will put the chart which is in the light DOM into the shadow root.
+        // The slot will put the chart which is in the light DOM into the shadow root.
+        return html`
+            <div>
+                <slot></slot>
+                <sl-button @click=${this.add_data}>Add Data</sl-button>
+            </div>
+            `;
     }
 
-    protected firstUpdated(_changedProperties: PropertyValues): void {
+    add_data() {
+        const now = new Date();
+        this.labels.push(now.getSeconds().toString())
+        this.series.forEach(({ data }) => {
+            data.push((Math.random() - 0.4) * 10 + (data[data.length - 1] ?? 0));
+        })
+        if (this.chart) {
+            update_chart(this.chart, this.labels, this.series)
+        }
+    }
+
+
+    firstUpdated(): void {
         // Create the echarts instance
         this.chart = echarts.init(this);
 
         // Draw the chart
         this.chart.setOption({
             title: {
-                text: 'ECharts Getting Started Example'
+                text: 'RL Agent Training Run'
             },
+            yAxis: {
+                boundaryGap: [0, '50%'],
+                type: 'value'
+              },
             tooltip: {},
             xAxis: {
-                data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+                data: []
             },
-            yAxis: {},
-            series: [
-                {
-                    name: 'sales',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }
-            ]
         });
 
         // Without this the chart sizing wouldn't work properly.
