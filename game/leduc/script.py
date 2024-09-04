46f495c7a1b96552a75bb26e4302dd6052db4d2c
mrakgr <mrakgr@gmail.com>
2024-09-04 16:48:26 +0200

    I keep wanting for somebody or something to save me from the fate of having to go forward with this, but that is not to be.
    We're going to have to run this.
    
    So the next step would be to integrate this with the UI.
    
    We have no choice to go forward even if what awaits us is destruction.
    
    We implemented everything that we need on the backend.
    
    After the break we'll take a look at the UI.

--- game/leduc/script.py
+++ game/leduc/script.py
@@ -1,13 +1,8 @@
-# import cupy as cp
+import cupy as cp
 
-# # Set the random seed
-# cp.random.seed(42)
+# Set the random seed
+cp.random.seed(42)
 
-# # Generate random numbers
-# random_array = cp.random.rand(3, 2)
-# print(random_array)
-
-from pathlib import Path
-file_path = Path("test_text_outputs/qwe/asd.txt")
-file_path.parent.mkdir(parents=True, exist_ok=True)
-file_path.write_text("Hello, world!")
+# Generate random numbers
+random_array = cp.random.rand(4, 3, 2)
+print(type(random_array.get()))
