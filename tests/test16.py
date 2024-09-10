8a1b373fd7eabd8ce5e7ec86dad3cfb3add88110
mrakgr <mrakgr@gmail.com>
2024-09-10 09:12:04 +0200

    Here is a simplified example.

---
+++ tests/test16.py
@@ -0,0 +1,132 @@
+kernel = r"""
+#include <new>
+#include <assert.h>
+#include <stdio.h>
+
+struct Union0;
+struct Union0_0 { // None
+};
+struct Union0_1 { // Some
+    int v0;
+    __device__ Union0_1(int t0) : v0(t0) {}
+    __device__ Union0_1() = delete;
+};
+struct Union0 {
+    union {
+        Union0_0 case0; // None
+        Union0_1 case1; // Some
+    };
+    unsigned char tag{255};
+    __device__ Union0() {}
+    __device__ Union0(Union0_0 t) : tag(0), case0(t) {} // None
+    __device__ Union0(Union0_1 t) : tag(1), case1(t) {} // Some
+    __device__ Union0(Union0 & x) : tag(x.tag) {
+        switch(x.tag){
+            case 0: new (&this->case0) Union0_0(x.case0); break; // None
+            case 1: new (&this->case1) Union0_1(x.case1); break; // Some
+        }
+    }
+    __device__ Union0(Union0 && x) : tag(x.tag) {
+        switch(x.tag){
+            case 0: new (&this->case0) Union0_0(std::move(x.case0)); break; // None
+            case 1: new (&this->case1) Union0_1(std::move(x.case1)); break; // Some
+        }
+    }
+    __device__ Union0 & operator=(Union0 & x) {
+        if (this->tag == x.tag) {
+            switch(x.tag){
+                case 0: this->case0 = x.case0; break; // None
+                case 1: this->case1 = x.case1; break; // Some
+            }
+        } else {
+            this->~Union0();
+            new (this) Union0{x};
+        }
+        return *this;
+    }
+    __device__ Union0 & operator=(Union0 && x) {
+        if (this->tag == x.tag) {
+            switch(x.tag){
+                case 0: this->case0 = std::move(x.case0); break; // None
+                case 1: this->case1 = std::move(x.case1); break; // Some
+            }
+        } else {
+            this->~Union0();
+            new (this) Union0{std::move(x)};
+        }
+        return *this;
+    }
+    __device__ ~Union0() {
+        switch(this->tag){
+            case 0: this->case0.~Union0_0(); break; // None
+            case 1: this->case1.~Union0_1(); break; // Some
+        }
+        this->tag = 255;
+    }
+};
+extern "C" __global__ void entry0() {
+    int v0;
+    v0 = threadIdx.x;
+    int v1;
+    v1 = blockIdx.x;
+    int v2;
+    v2 = v1 * 256l;
+    int v3;
+    v3 = v0 + v2;
+    static Union0 v4[6144l];
+    Union0 v5;
+    v5 = Union0{Union0_1{1l}};
+    v4[v3] = v5;
+    int v6;
+    v6 = threadIdx.x;
+    int v7;
+    v7 = blockIdx.x;
+    int v8;
+    v8 = v7 * 256l;
+    int v9;
+    v9 = v6 + v8;
+    Union0 v10;
+    v10 = v4[v9];
+    return ;
+}
+"""
+import cupy as cp
+from dataclasses import dataclass
+from typing import NamedTuple, Union, Callable, Tuple
+i8 = int; i16 = int; i32 = int; i64 = int; u8 = int; u16 = int; u32 = int; u64 = int; f32 = float; f64 = float; char = str; string = str
+
+options = []
+options.append('--define-macro=NDEBUG')
+options.append('--dopt=on')
+options.append('--diag-suppress=550,20012,68,39,177')
+options.append('--restrict')
+options.append('--maxrregcount=256')
+options.append('--std=c++20')
+options.append('-D__CUDA_NO_HALF_CONVERSIONS__')
+raw_module = cp.RawModule(code=kernel, backend='nvcc', enable_cooperative_groups=True, options=tuple(options))
+def main_body():
+    v0 = cp.cuda.Device().attributes['MultiProcessorCount']
+    v1 = v0 == 24
+    del v0
+    v2 = v1 == False
+    if v2:
+        v3 = "The number of SMs per GPU at runtime must much that what is declared atop of corecuda.base. Make sure to use the correct constant so it can be propagated at compile time."
+        assert v1, v3
+        del v3
+    else:
+        pass
+    del v1, v2
+    v4 = 0
+    v5 = raw_module.get_function(f"entry{v4}")
+    del v4
+    v5.max_dynamic_shared_size_bytes = 81920 
+    v5((24,),(256,),(),shared_mem=81920)
+    del v5
+    return 
+
+def main():
+    r = main_body()
+    cp.cuda.get_current_stream().synchronize() # This line is here so the `__trap()` calls on the kernel aren't missed.
+    return r
+
+if __name__ == '__main__': print(main())
