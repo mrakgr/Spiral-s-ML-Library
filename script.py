b9d82f4f08e8b16a985e7ac32267c1e8c1f707db
mrakgr <mrakgr@gmail.com>
2024-10-22 18:04:06 +0200

    Did some light prototyping of Hopfield dictionaries today. I meant to do further work and check out Jax, but that will have to wait for later. Tomorrow, most likely I'll be busy playing with inference servers.

--- script.py
+++ script.py
@@ -1,4 +1,37 @@
 import cupy as cp
 import cupyx
-x = cp.array([1,2,3])
-print(cupyx.scipy.special.softmax(x*0.01))
+
+T = 10
+def softmax(x : cp.ndarray) -> cp.ndarray : return cupyx.scipy.special.softmax(x * T)
+
+m = 2 ** 20
+n = 2
+k = 1
+keys = cp.zeros((m,n))
+values = cp.zeros((m,k))
+
+i = 0
+def add(k,v):
+    global i
+    keys[i] = cp.array(k)
+    values[i] = cp.array(v)
+    i += 1
+
+add([0,1],[[1]])
+add([1,0],[[1]])
+add([1,1],[[-1]])
+
+# print(keys)
+# print(values)
+
+def index_keys(x):
+    x = x.T
+    return cp.matmul(keys,x)
+def index_values(x):
+    x = cp.array(x).T
+    return cp.matmul(x,values)
+
+key = cp.random.random((2 ** 10,n))
+s = index_keys(key)
+print(index_values(softmax(s)))
+
