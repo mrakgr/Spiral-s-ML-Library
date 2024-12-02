72f81beff4292240a5d49722629b8dcec7111550
mrakgr <marko@taoceti.ai>
2024-12-02 13:08:57 +0000

    .

--- tests/distributed_shared_memory/test0.py
+++ tests/distributed_shared_memory/test0.py
@@ -1,21 +1,15 @@
 kernel = r"""
-extern "C" __global__ void __cluster_dims__(4,1,1) entry0() {
+extern "C" __global__ void __cluster_dims__(8,1,1) entry0() {
     return ;
 }
 """
 import cupy as cp
 
-raw_module = cp.RawModule(code=kernel, backend='nvcc', enable_cooperative_groups=True)
-def main_body():
+raw_module = cp.RawModule(code=kernel, backend='nvcc', enable_cooperative_groups=False)
+def main():
     v9 = raw_module.get_function(f"entry0")
     v9.max_dynamic_shared_size_bytes = 229376 
-    print(f'Threads per block, blocks per grid: {256}, {128}')
     v9((128,),(256,),(),shared_mem=229376)
     return 
-
-def main():
-    r = main_body()
-    cp.cuda.get_current_stream().synchronize() # This line is here so the `__trap()` calls on the kernel aren't missed.
-    return r
 
 if __name__ == '__main__': print(main())
