42963b427c0bf1be261e04f899503f58b03cd3bc
mrakgr <marko@taoceti.ai>
2024-12-21 16:01:56 +0000

    Added support for thread block clusters and distributed shared memory to the ML library.

---
+++ tests/distributed_shared_memory/test1.py
@@ -0,0 +1,381 @@
+kernel = r"""
+#include <new>
+#include <assert.h>
+#include <stdio.h>
+#include <cooperative_groups.h>
+#include <cuda/semaphore>
+__device__ cuda::binary_semaphore<cuda::thread_scope_system> console_lock(1);
+using default_int = int;
+using default_uint = unsigned int;
+template <typename el>
+struct sptr // Shared pointer for the Spiral datatypes. They have to have the refc field inside them to work.
+{
+    el* base;
+
+    __device__ sptr() : base(nullptr) {}
+    __device__ sptr(el* ptr) : base(ptr) { this->base->refc++; }
+
+    __device__ ~sptr()
+    {
+        if (this->base != nullptr && --this->base->refc == 0)
+        {
+            delete this->base;
+            this->base = nullptr;
+        }
+    }
+
+    __device__ sptr(sptr& x)
+    {
+        this->base = x.base;
+        this->base->refc++;
+    }
+
+    __device__ sptr(sptr&& x)
+    {
+        this->base = x.base;
+        x.base = nullptr;
+    }
+
+    __device__ sptr& operator=(sptr& x)
+    {
+        if (this->base != x.base)
+        {
+            delete this->base;
+            this->base = x.base;
+            this->base->refc++;
+        }
+        return *this;
+    }
+
+    __device__ sptr& operator=(sptr&& x)
+    {
+        if (this->base != x.base)
+        {
+            delete this->base;
+            this->base = x.base;
+            x.base = nullptr;
+        }
+        return *this;
+    }
+};
+
+template <typename el>
+struct csptr : public sptr<el>
+{ // Shared pointer for closures specifically.
+    using sptr<el>::sptr;
+    template <typename... Args>
+    __device__ auto operator()(Args... args) -> decltype(this->base->operator()(args...))
+    {
+        return this->base->operator()(args...);
+    }
+};
+
+template <typename el, default_int max_length>
+struct static_array
+{
+    el ptr[max_length];
+    __device__ el& operator[](default_int i) {
+        assert("The index has to be in range." && 0 <= i && i < max_length);
+        return this->ptr[i];
+    }
+};
+
+template <typename el, default_int max_length>
+struct static_array_list
+{
+    default_int length{ 0 };
+    el ptr[max_length];
+
+    __device__ el& operator[](default_int i) {
+        assert("The index has to be in range." && 0 <= i && i < this->length);
+        return this->ptr[i];
+    }
+    __device__ void push(el& x) {
+        ptr[this->length++] = x;
+        assert("The array after pushing should not be greater than max length." && this->length <= max_length);
+    }
+    __device__ void push(el&& x) {
+        ptr[this->length++] = std::move(x);
+        assert("The array after pushing should not be greater than max length." && this->length <= max_length);
+    }
+    __device__ el pop() {
+        assert("The array before popping should be greater than 0." && 0 < this->length);
+        auto x = ptr[--this->length];
+        ptr[this->length].~el();
+        new (&ptr[this->length]) el();
+        return x;
+    }
+    // Should be used only during initialization.
+    __device__ void unsafe_set_length(default_int i) {
+        assert("The new length should be in range." && 0 <= i && i <= max_length);
+        this->length = i;
+    }
+};
+
+template <typename el, default_int max_length>
+struct dynamic_array_base
+{
+    int refc{ 0 };
+    el* ptr;
+
+    __device__ dynamic_array_base() : ptr(new el[max_length]) {}
+    __device__ ~dynamic_array_base() { delete[] this->ptr; }
+
+    __device__ el& operator[](default_int i) {
+        assert("The index has to be in range." && 0 <= i && i < this->length);
+        return this->ptr[i];
+    }
+};
+
+template <typename el, default_int max_length>
+struct dynamic_array
+{
+    sptr<dynamic_array_base<el, max_length>> ptr;
+
+    __device__ dynamic_array() = default;
+    __device__ dynamic_array(bool t) : ptr(new dynamic_array_base<el, max_length>()) {}
+    __device__ el& operator[](default_int i) {
+        return this->ptr.base->operator[](i);
+    }
+};
+
+template <typename el, default_int max_length>
+struct dynamic_array_list_base
+{
+    int refc{ 0 };
+    default_int length{ 0 };
+    el* ptr;
+
+    __device__ dynamic_array_list_base() : ptr(new el[max_length]) {}
+    __device__ dynamic_array_list_base(default_int l) : ptr(new el[max_length]) { this->unsafe_set_length(l); }
+    __device__ ~dynamic_array_list_base() { delete[] this->ptr; }
+
+    __device__ el& operator[](default_int i) {
+        assert("The index has to be in range." && 0 <= i && i < this->length);
+        return this->ptr[i];
+    }
+    __device__ void push(el& x) {
+        ptr[this->length++] = x;
+        assert("The array after pushing should not be greater than max length." && this->length <= max_length);
+    }
+    __device__ void push(el&& x) {
+        ptr[this->length++] = std::move(x);
+        assert("The array after pushing should not be greater than max length." && this->length <= max_length);
+    }
+    __device__ el pop() {
+        assert("The array before popping should be greater than 0." && 0 < this->length);
+        auto x = ptr[--this->length];
+        ptr[this->length].~el();
+        new (&ptr[this->length]) el();
+        return x;
+    }
+    // Should be used only during initialization.
+    __device__ void unsafe_set_length(default_int i) {
+        assert("The new length should be in range." && 0 <= i && i <= max_length);
+        this->length = i;
+    }
+};
+
+template <typename el, default_int max_length>
+struct dynamic_array_list
+{
+    sptr<dynamic_array_list_base<el, max_length>> ptr;
+
+    __device__ dynamic_array_list() = default;
+    __device__ dynamic_array_list(default_int l) : ptr(new dynamic_array_list_base<el, max_length>(l)) {}
+
+    __device__ el& operator[](default_int i) {
+        return this->ptr.base->operator[](i);
+    }
+    __device__ void push(el& x) {
+        this->ptr.base->push(x);
+    }
+    __device__ void push(el&& x) {
+        this->ptr.base->push(std::move(x));
+    }
+    __device__ el pop() {
+        return this->ptr.base->pop();
+    }
+    // Should be used only during initialization.
+    __device__ void unsafe_set_length(default_int i) {
+        this->ptr.base->unsafe_set_length(i);
+    }
+    __device__ default_int length_() {
+        return this->ptr.base->length;
+    }
+};
+
+__device__ inline bool while_method_0(int v0){
+    bool v1;
+    v1 = v0 < 16;
+    return v1;
+}
+extern "C" __global__ void __cluster_dims__(8,1,1) entry0() {
+    auto v0 = cooperative_groups::this_cluster();
+    extern __shared__ unsigned char v1[];
+    float * v2;
+    v2 = reinterpret_cast<float *>(&v1[0ull]);
+    int v4;
+    v4 = threadIdx.x;
+    int v5;
+    v5 = v4;
+    while (while_method_0(v5)){
+        bool v7;
+        v7 = 0 <= v5;
+        bool v8;
+        v8 = v7 == false;
+        if (v8){
+            assert("The index needs to be zero or positive." && v7);
+        } else {
+        }
+        int v10;
+        v10 = v5 % 4;
+        int v11;
+        v11 = v5 / 4;
+        int v12;
+        v12 = v11 % 2;
+        int v13;
+        v13 = v11 / 2;
+        bool v14;
+        v14 = v13 < 2;
+        bool v15;
+        v15 = v14 == false;
+        if (v15){
+            assert("The last element of the projection dimensions needs to be greater than the index remainder." && v14);
+        } else {
+        }
+        int v17;
+        v17 = blockIdx.x;
+        float v18;
+        v18 = (float)v17;
+        assert("Tensor range check" && 0 <= v13 && v13 < 2);
+        assert("Tensor range check" && 0 <= v12 && v12 < 2);
+        assert("Tensor range check" && 0 <= v10 && v10 < 4);
+        int v19;
+        v19 = 4 * v12;
+        int v20;
+        v20 = v19 + v10;
+        int v21;
+        v21 = 8 * v13;
+        int v22;
+        v22 = v21 + v20;
+        v2[v22] = v18;
+        v5 += 256 ;
+    }
+    v0.sync() ;
+    float * v23;
+    v23 = v2+0;
+    float * v25;
+    v25 = v0.map_shared_rank(v23,7);
+    float v26;
+    v26 = v25[15];
+    int v27;
+    v27 = threadIdx.x;
+    bool v28;
+    v28 = v27 == 0;
+    if (v28){
+        int v29;
+        v29 = blockIdx.x;
+        cuda::counting_semaphore<cuda::thread_scope_system, 1> & v30 = console_lock;
+        auto v31 = cooperative_groups::coalesced_threads();
+        v30.acquire();
+        printf("{%s = %d; %s = %f}\n","bid", v29, "v", v26);
+        v30.release();
+        v31.sync() ;
+    } else {
+    }
+    return ;
+}
+"""
+class static_array():
+    def __init__(self, length):
+        self.ptr = []
+        for _ in range(length):
+            self.ptr.append(None)
+
+    def __getitem__(self, index):
+        assert 0 <= index < len(self.ptr), "The get index needs to be in range."
+        return self.ptr[index]
+    
+    def __setitem__(self, index, value):
+        assert 0 <= index < len(self.ptr), "The set index needs to be in range."
+        self.ptr[index] = value
+
+class static_array_list(static_array):
+    def __init__(self, length):
+        super().__init__(length)
+        self.length = 0
+
+    def __getitem__(self, index):
+        assert 0 <= index < self.length, "The get index needs to be in range."
+        return self.ptr[index]
+    
+    def __setitem__(self, index, value):
+        assert 0 <= index < self.length, "The set index needs to be in range."
+        self.ptr[index] = value
+
+    def push(self,value):
+        assert (self.length < len(self.ptr)), "The length before pushing has to be less than the maximum length of the array."
+        self.ptr[self.length] = value
+        self.length += 1
+
+    def pop(self):
+        assert (0 < self.length), "The length before popping has to be greater than 0."
+        self.length -= 1
+        return self.ptr[self.length]
+
+    def unsafe_set_length(self,i):
+        assert 0 <= i <= len(self.ptr), "The new length has to be in range."
+        self.length = i
+
+class dynamic_array(static_array): 
+    pass
+
+class dynamic_array_list(static_array_list):
+    def length_(self): return self.length
+
+import cupy as cp
+from dataclasses import dataclass
+from typing import NamedTuple, Union, Callable, Tuple
+i8 = int; i16 = int; i32 = int; i64 = int; u8 = int; u16 = int; u32 = int; u64 = int; f32 = float; f64 = float; char = str; string = str
+
+options = []
+options.append('--define-macro=NDEBUG')
+options.append('--dopt=on')
+options.append('--diag-suppress=550,20012,68,39,177')
+options.append('--restrict')
+options.append('--maxrregcount=255')
+options.append('--std=c++20')
+options.append('-D__CUDA_NO_HALF_CONVERSIONS__')
+raw_module = cp.RawModule(code=kernel, backend='nvcc', enable_cooperative_groups=True, options=tuple(options))
+def main_body():
+    v2 = "{}\n"
+    v3 = "Can we access  shared memory?"
+    print(v2.format(v3),end="")
+    del v2, v3
+    v4 = cp.cuda.Device().attributes['MultiProcessorCount']
+    v5 = v4 >= 112
+    del v4
+    v6 = v5 == False
+    if v6:
+        v7 = "The number of SMs per GPU at runtime must much that what is declared atop of corecuda.base. Make sure to use the correct constant so it can be propagated at compile time."
+        assert v5, v7
+        del v7
+    else:
+        pass
+    del v5, v6
+    v8 = 0
+    v9 = raw_module.get_function(f"entry{v8}")
+    del v8
+    v9.max_dynamic_shared_size_bytes = 229376 
+    print(f'Threads per block, blocks per grid: {256}, {112}')
+    v9((112,),(256,),(),shared_mem=229376)
+    del v9
+    return 
+
+def main():
+    r = main_body()
+    cp.cuda.get_current_stream().synchronize() # This line is here so the `__trap()` calls on the kernel aren't missed.
+    return r
+
+if __name__ == '__main__': print(main())
