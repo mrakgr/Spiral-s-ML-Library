7497b54e9cf1ae6aa0f1f79d44162460a5b3e0e3
mrakgr <marko@taoceti.ai>
2025-01-26 14:37:25 +0000

    The debugger works on both the host and the GPU. Let me try running this in WSL.

--- cpp_tests/hello.cu
+++ cpp_tests/hello.cu
@@ -2,11 +2,18 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include "kittens.cuh"
-using namespace kittens;
+// #include "kittens.cuh"
+// using namespace kittens;
+
+__global__ void test_print(){
+    printf("Hello from inside the kernel.\n");
+    return;
+}
 
 int main() {
     int i = 0;
     printf("Hello. %i\n", i);
+    test_print<<<1,1>>>();
+    cudaDeviceSynchronize();
     return 0;
 }
