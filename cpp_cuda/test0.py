f7b62e64021b2459e7f1a01ffd5cc8ded647efba
mrakgr <mrakgr@gmail.com>
2025-03-09 18:12:02 +0100

    The device sync part at the end of the kernel works now.

---
+++ cpp_cuda/test0.py
@@ -0,0 +1,72 @@
+kernels_main = r"""
+#include <cooperative_groups.h>
+#include <cuda/semaphore>
+__device__ cuda::binary_semaphore<cuda::thread_scope_system> console_lock(1);
+extern "C" __global__ void entry0() {
+    int v0;
+    v0 = 5;
+    int v1;
+    v1 = 3;
+    int v2;
+    v2 = v0 + v1;
+    cuda::counting_semaphore<cuda::thread_scope_system, 1> & v3 = console_lock;
+    auto v4 = cooperative_groups::coalesced_threads();
+    v3.acquire();
+    printf("%s\n","hello");
+    v3.release();
+    v4.sync() ;
+    return ;
+}
+"""
+from test0_auto import *
+kernels = kernels_aux + kernels_main
+import cupy as cp
+from dataclasses import dataclass
+from typing import NamedTuple, Union, Callable, Tuple
+i8 = int; i16 = int; i32 = int; i64 = int; u8 = int; u16 = int; u32 = int; u64 = int; f32 = float; f64 = float; char = str; string = str
+
+options = []
+options.append('--define-macro=NDEBUG')
+options.append('--dopt=on')
+options.append('--diag-suppress=550,20012,68,39,177')
+options.append('--restrict')
+import os
+home = os.getenv('HOME')
+options.append(f'-I={home}/ThunderKittens/include')
+options.append('--std=c++20')
+options.append('--expt-relaxed-constexpr')
+options.append('-D__CUDA_NO_HALF_CONVERSIONS__')
+raw_module = cp.RawModule(code=kernels, backend='nvcc', enable_cooperative_groups=True, options=tuple(options))
+def method0() -> None:
+    kernel = "entry0"
+    v0 = cp.cuda.Device().attributes['MultiProcessorCount']
+    v1 = v0 >= 1
+    del v0
+    v2 = v1 == False
+    if v2:
+        v3 = "The number of SMs per GPU at runtime must much that what is declared atop of corecuda.base. Make sure to use the correct constant so it can be propagated at compile time."
+        assert v1, v3
+        del v3
+    else:
+        pass
+    del v1, v2
+    v4 = raw_module.get_function(kernel)
+    v4.max_dynamic_shared_size_bytes = 98304 
+    print(f'Threads per block, blocks per grid: {1}, {1}')
+    v4((1,),(1,),(),shared_mem=98304)
+    del v4
+    return 
+def main_body():
+    v0 = 2
+    v1 = 8
+    v2 = v0 * v1
+    del v0, v1, v2
+    method0()
+    return 0
+
+def main():
+    r = main_body()
+    cp.cuda.get_current_stream().synchronize() # This line is here so the `__trap()` calls on the kernel aren't missed.
+    return r
+
+if __name__ == '__main__': print(main())
