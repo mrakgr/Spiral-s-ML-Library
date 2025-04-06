9bbe8adce1c881f97c3a5bc128d996719a6fdb4a
Marko Grdinić <mrakgr@gmail.com>
2025-04-06 15:18:07 +0200

    Nice. Thrust comes out of the box with the latest Cuda Toolkit. I'll use the device vectors as native arrays in the backend.

--- cpp_tests/build.ps1
+++ cpp_tests/build.ps1
@@ -1,12 +1,16 @@
 $WarningPreference = 'SilentlyContinue'; $ErrorActionPreference = "Stop"; Set-StrictMode -Version Latest
 
 $path_input = "./hello.cu"
-$path_output = Join-Path (Split-Path $path_input -Parent) "bin" (Split-Path $path_input -LeafBase)
+$path_output_dir = Join-Path (Split-Path $path_input -Parent) "bin"
+if (-not (Test-Path $path_output_dir)) {
+    New-Item $path_output_dir -ItemType Directory
+}
+$path_output = Join-Path $path_output_dir (Split-Path $path_input -LeafBase)
 
 if (-not (Test-Path $path_output) -or ((Get-Item $path_input).CreationTime -ge (Get-Item $path_output).CreationTime)) {
     Write-Host "Compiling '$path_input' into '$path_output'"
     nvcc `
-    -arch=sm_89 `
+    -arch=sm_120 `
     -D=NDEBUG `
     -g -G `
     -dopt=on `
@@ -16,11 +20,13 @@ if (-not (Test-Path $path_output) -or ((Get-Item $path_input).CreationTime -ge (
     -std=c++20 `
     -expt-relaxed-constexpr `
     -D__CUDA_NO_HALF_CONVERSIONS__ `
-    -o (New-Item $path_output -Force) `
+    -o $path_output `
     $path_input
 } else {
     Write-Host "The '$path_output' is up to date."
 }
+
+& "$path_output"
 
 <#
 pwsh build.ps1
--- cpp_tests/hello.cu
+++ cpp_tests/hello.cu
@@ -1,19 +1,62 @@
-#include <new>
-#include <assert.h>
-#include <stdio.h>
+// #include <new>
+// #include <assert.h>
+// #include <stdio.h>
 
-// #include "kittens.cuh"
-// using namespace kittens;
+// // #include "kittens.cuh"
+// // using namespace kittens;
 
-__global__ void test_print(){
-    printf("Hello from inside the kernel.\n");
-    return;
-}
+// __global__ void test_print(){
+//     printf("Hello from inside the kernel.\n");
+//     return;
+// }
 
-int main() {
-    int i = 0;
-    printf("Hello. %i\n", i);
-    test_print<<<1,1>>>();
-    cudaDeviceSynchronize();
+// int main() {
+//     int i = 0;
+//     printf("Hello. %i\n", i);
+//     test_print<<<1,1>>>();
+//     cudaDeviceSynchronize();
+//     return 0;
+// }
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include <iostream>
+
+int main(void)
+{
+    // H has storage for 4 integers
+    thrust::host_vector<int> H(4);
+
+    // initialize individual elements
+    H[0] = 14;
+    H[1] = 20;
+    H[2] = 38;
+    H[3] = 46;
+
+    // H.size() returns the size of vector H
+    std::cout << "H has size " << H.size() << std::endl;
+
+    // print contents of H
+    for(int i = 0; i < H.size(); i++)
+        std::cout << "H[" << i << "] = " << H[i] << std::endl;
+
+    // resize H
+    H.resize(2);
+
+    std::cout << "H now has size " << H.size() << std::endl;
+
+    // Copy host_vector H to device_vector D
+    thrust::device_vector<int> D = H;
+
+    // elements of D can be modified
+    D[0] = 99;
+    D[1] = 88;
+
+    // print contents of D
+    for(int i = 0; i < D.size(); i++)
+        std::cout << "D[" << i << "] = " << D[i] << std::endl;
+
+    // H and D are automatically deleted when the function returns
     return 0;
 }
