b9e92ae7b396e3b3924e5eed25f5cb3d2b47e9c7
Marko Grdinić <mrakgr@gmail.com>
2025-11-01 17:29:56 +0100

    Will change these arrays to Eigen vectors tomorrow.

--- hopfield_dictionary/host_cpp_lib/hopfield_dict.cpp
+++ hopfield_dictionary/host_cpp_lib/hopfield_dict.cpp
@@ -5,26 +5,14 @@
 // For more info see: https://libeigen.gitlab.io/eigen/docs-nightly/TopicCUDA.html
 
 // matmul(((matmul(keys, input.T) - input.sum) / temperature).T, values)
-
 // Returns a value (into the output matrix) given the input key from the hopfield dictionary.
-
-// void qwerasdf(){}
-
-// template <typename t, int rows, int cols_keys, int cols_values, int rows_input>
-// void hopfield_dictionary_get(
-//         Eigen::Matrix<t,rows_input,cols_keys> & input, 
-//         Eigen::Matrix<t,rows,cols_keys> & keys, 
-//         Eigen::Matrix<t,rows,cols_values> & values, 
-//         Eigen::Matrix<t,rows_input,cols_values> & out, 
-//         t temperature) {
-//     out = ((keys * input.transpose() - input.rowwise().sum().transpose().replicate(keys.rows(),1)).array() / temperature).exp().matrix().transpose() * values;
-// }
-
-template <int i>
-void qwer() {
-    printf("%i\n", i);
+template <typename t, int rows, int cols_keys, int cols_values, int rows_input>
+void hopfield_dictionary_get(
+        Eigen::Matrix<t,rows_input,cols_keys> & input, 
+        Eigen::Matrix<t,rows,cols_keys> & keys, 
+        Eigen::Matrix<t,rows,cols_values> & values, 
+        Eigen::Matrix<t,rows_input,cols_values> & out, 
+        t temperature) {
+    out = ((keys * input.transpose() - input.rowwise().sum().transpose().replicate(keys.rows(),1)).array() / temperature).exp().matrix().transpose() * values;
 }
 
-void bar(Foo & x) {
-    x.hi();
-}
--- hopfield_dictionary/host_cpp_lib/hopfield_dict.hpp
+++ hopfield_dictionary/host_cpp_lib/hopfield_dict.hpp
@@ -1,24 +1,4 @@
 #pragma once
 
-#include <stdio.h>
-// #include <Eigen/Dense>
+#include <Eigen/Dense>
 
-#ifndef __host__
-#define __host__
-#endif
-
-#ifndef __device__
-#define __device__
-#endif
-
-struct Foo {
-    int i = 5;
-    __host__ __device__ void hi() {
-        printf("Hi. %i\n", i);
-    }
-};
-
-void bar(Foo & x);
-
-template <int i>
-void qwer();
