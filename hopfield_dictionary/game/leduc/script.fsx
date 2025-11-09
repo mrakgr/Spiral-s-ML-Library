a71b1bfdf63f111b2b66df680a3663c7301d6d80
Marko Grdinić <mrakgr@gmail.com>
2025-11-09 09:26:28 +0100

    Save.

--- hopfield_dictionary/game/leduc/script.fsx
+++ hopfield_dictionary/game/leduc/script.fsx
@@ -1,20 +1,8 @@
-let x = 
-    [1;2;3;4;5] 
-    |> List.map (fun x -> System.Random.Shared.NextDouble(), x)
-    |> List.sortBy fst
-    |> List.map snd
-
-// let take_max (l : int list) =
-//     l
-//     |> List.mapi (fun i x -> x, System.Random.Shared.NextDouble(), i)
-//     |> List.max
-//     |> fun (_,_,i) -> i
-
-let take_max (l : int list) =
+let arg_max (l : int list) =
     l
-    |> List.mapi (fun i x -> x, i)
+    |> List.mapi (fun i x -> x, System.Random.Shared.NextDouble(), i)
     |> List.max
-    |> fun (_,i) -> i
+    |> fun (_,_,i) -> i
 
-take_max [1;2;3;4;5;5;5;5;5;5]
+arg_max [1;2;3;4;5;5;5;5;5;5]
 
