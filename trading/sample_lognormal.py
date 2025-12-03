3de45d1e6df2866de94552b9df47a2d04ea55f6c
Marko Grdinić <mrakgr@gmail.com>
2025-12-03 11:19:12 +0100

    Tried displaying some NVDA intraday data with mplfinance library.

--- trading/sample_lognormal.py
+++ trading/sample_lognormal.py
@@ -2,19 +2,13 @@ import numpy as np
 
 def sample_int_lognormal(mean, sigma, n):
     mu = np.log(mean) - 0.5 * sigma**2
-    x = np.random.lognormal(mu, sigma, size=n)
-    f = np.floor(x)
-    r = np.random.rand(n)
-    return f + (r < (x - f))
-
-def sample_int_lognormal_my(mean, sigma, n):
-    mu = np.log(mean) - 0.5 * sigma**2
+    # x = np.random.lognormal(mu, sigma, size=n) # These two lines are the same.
     x = np.exp(np.random.normal(mu, sigma, size=n))
     f = np.floor(x)
     r = np.random.rand(n)
     return f + (r < (x - f))
 
 # Example:
-samples = sample_int_lognormal_my(mean=10, sigma=0.6, n=100)
+samples = sample_int_lognormal(mean=20, sigma=0.6, n=100)
 print(np.mean(samples)) # Should be close to 20
 print(samples) # Should be close to 20
